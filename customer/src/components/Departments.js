db1cf1d3fc364a8b0e7d53c640bf86d5f8b4f7fa
Kaleb Allen <7kaleb29allen@gmail.com>
2019-03-19 16:26:43 -0600

    navbar and departments page

--- customer/src/components/Departments.js
+++ customer/src/components/Departments.js
@@ -1,6 +1,44 @@
 import React from 'react';
-import {Header, } from 'semantic-ui-react';
+import {Header, Card, } from 'semantic-ui-react';
 
 class Departments extends React.Component {
-    state = {departments: [], }
+    state = {departments: [], };
+
+    componentDidMount() {
+
+    }
+
+    renderDepartments = () => {
+        const {departments, } = this.state
+
+        if (departments.length <= 0)
+            return <h3>Out of Stock. Sorry!!!</h3>
+        return departments.map( department => (
+            <Card>
+                <Card.Content>
+                    <Card.Header>{ department.name }</Card.Header>
+                </Card.Content>
+            </Card>
+        ))
+    }
+
+    render() {
+        return (
+            <div>
+                <Header as='h1'>Departments</Header>
+                <br />
+                <br />
+                <br />
+                <Card.Group>
+                    { this.renderDepartments() }
+                </Card.Group>
+            </div>
+        )
+    }
+
+   
 }
+
+
+
+export default Departments;
