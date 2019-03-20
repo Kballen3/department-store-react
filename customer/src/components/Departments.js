1179c3d842f951e63d480c6db4c46127f59faa9c
Kaleb Allen <7kaleb29allen@gmail.com>
2019-03-20 16:28:19 -0600

    style and delete

--- customer/src/components/Departments.js
+++ customer/src/components/Departments.js
@@ -13,22 +13,51 @@ class Departments extends React.Component {
             })
     }
 
+    handleUpdate = (id) => {
+        axios.put(`/api/departments/${id}`)
+            .then( res => {
+                const departments = this.state.departments.map( t=>{
+                    if (t.id === id)
+                        return res.data
+                    return t;
+                })
+                this.setState({departments})
+            })
+    }
+
+    handleDelete = (id) => {
+        axios.delete(`/api/departments/${id}`)
+            .then( res => {
+                const {departments, } = this.state
+                this.setState({departments: departments.filter(t => t.id !== id), })
+            })
+            
+    }
+
     renderDepartments = () => {
         const {departments, } = this.state
 
         if (departments.length <= 0)
             return <h3>Out of Stock. Sorry!!!</h3>
         return departments.map( department => (
-            <Card>
-                <Card.Content>
-                    <Card.Header>{ department.name }</Card.Header>
-                </Card.Content>
-                <Card.Content extra>
-                    <Button as={Link} to={`/departments/${department.id}`} color='blue'>
-                        View
-                    </Button>
-                </Card.Content>
-            </Card>
+            <div>
+                <Card>
+                    <Card.Content>
+                        <Card.Header>{ department.name }</Card.Header>
+                    </Card.Content>
+                    <Card.Content extra>
+                        <Button as={Link} to={`/departments/${department.id}`} color='blue'>
+                            View
+                        </Button>
+                        <Button onClick={() => this.handleDelete(department.id)} color="red">
+                            Delete
+                        </Button>
+                        <Button onClick={this.handleUpdate} color="pink">
+                            Edit
+                        </Button>
+                    </Card.Content>
+                </Card>
+            </div>
         ))
     }
 
